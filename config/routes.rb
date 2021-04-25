8945a9b339533bba0ee37732831bd19b244b9dc5
影山 慎太郎 <kagexa@gmail.com>
2021-04-25 15:04:05 +0000

    Finish 8.5.1

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   
   get    '/login', to: 'sessions#new'
+  get    'posts/new', to: 'posts#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get    'posts/:id', to: 'post#show', as: :post
   
-  resources :users
+  resources :users do
+    resources :tasks 
+  end
 end
